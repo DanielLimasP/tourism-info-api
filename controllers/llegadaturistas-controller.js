9ed9add7cac66aad763c1891c3a903c5d611249f
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-09-26 02:48:48 -0600

    Fixed some other things

--- controllers/llegadaturistas-controller.js
+++ controllers/llegadaturistas-controller.js
@@ -1,34 +1,34 @@
 const LlegadaTuristasModel = require("../models/Llegada-Turistas");
 
 async function getLlegadaTuristaFromYear(req, res) {
-    console.log("Body of the request", req.body);
-    let year = req.body.year;
-  
-    let result = await LlegadaTuristasdModel.find({ Año: year });
-  
-    return res
-      .status(200)
-      .send({ message: `Llegada de turistas from year ${year}`, concepts: result });
-  }
-  
-  async function createLlegadaTuristas(req, res) {
-    console.log(req.body);
-    let { año, municipio, region, meses } = req.body;
-  
-    const newLlegadaTuristas = new LlegadaTuristasModel({
-      año,
-      municipio,
-      region,
-      meses,
-    });
-    newLlegadaTuristas.save((err, llegadaturistasStored) => {
-      if (err) return res.status(501).send({ message: `Error on model ${err}` });
-      return res.status(200).send({ message: "Llegada de Turistas uploaded" });
-    });
-  }
-  
-  module.exports = {
-    getLlegadaTuristaFromYear,
-    createLlegadaTuristas,
-  };
-  
+  console.log("Body of the request", req.body);
+  let year = req.body.year;
+
+  let result = await LlegadaTuristasModel.find({ año: year });
+
+  return res.status(200).send({
+    message: `Llegada de turistas from year ${year}`,
+    concepts: result,
+  });
+}
+
+async function createLlegadaTuristas(req, res) {
+  console.log(req.body);
+  let { año, municipio, region, meses } = req.body;
+
+  const newLlegadaTuristas = new LlegadaTuristasModel({
+    año,
+    municipio,
+    region,
+    meses,
+  });
+  newLlegadaTuristas.save((err, llegadaturistasStored) => {
+    if (err) return res.status(501).send({ message: `Error on model ${err}` });
+    return res.status(200).send({ message: "Llegada de Turistas uploaded" });
+  });
+}
+
+module.exports = {
+  getLlegadaTuristaFromYear,
+  createLlegadaTuristas,
+};
