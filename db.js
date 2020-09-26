38ad8afacf3cd1173ae2b6199e4704e4c432bfd6
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-09-25 23:13:32 -0600

    db and server modules

---
+++ db.js
@@ -0,0 +1,17 @@
+// mondgo db connection file
+// we gonna use both local instance of mongo and atlas
+
+const mongoose = require("mongoose");
+
+const atlas = "admin"; // Ask for admin
+const local = "mongodb://localhost/iweek-hackaton-db";
+
+mongoose
+  .connect(local, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((db) => console.log("DB Connection established"))
+  .catch((error) => console.log(error));
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+//Initializations
+const app = express();
+require("./db");
+
+//Settings
+app.set("port", process.env.PORT || 420);
+
+//Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser({ limit: "50mb" }));
+app.use(bodyParser.json());
+
+//Routes
+// TODO: Define routes
+//const fooRoutes = require("./bar/route");
+//app.use("/foo", fooRoutes);
+
+//Static Files
+app.use(express.static(path.join(__dirname, "public")));
+
+//Server Initialize
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
