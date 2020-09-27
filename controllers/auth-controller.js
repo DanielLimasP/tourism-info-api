bb70c0448c374ace07b715ad13d56e37ece8ece5
miwelomali <49109441+miwelomali@users.noreply.github.com>
2020-09-26 19:47:33 -0600

    Fixed Username Parameteres at login

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -47,7 +47,7 @@ const login = (req, res, next) => {
                     })
                 }
                 if(result){
-                    let token = jwt.sign({name: user.name}, 'verySecretValue', {expiresIn: '1h'})
+                    let token = jwt.sign({name: user.username}, 'verySecretValue', {expiresIn: '1h'})
                     res.json({
                         message: 'Login Successfull',
                         token
